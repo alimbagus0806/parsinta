e999ddec5d963f16562cd58c2a35c2b06f300172
Bagus <alimbagus0806@gmail.com>
2023-01-25 08:47:50 +0700

    add

---
+++ baru/public/js/app.js
@@ -0,0 +1,1 @@
+// alert `Hello This is Laravel 8!`;
---
+++ baru/resources/views/profile.blade.php
@@ -0,0 +1,11 @@
+{{-- @extends('layouts.app') --}}
+@extends('layouts.app',['title'=>'Profile'])
+{{-- @section('title','Contact') --}}
+{{-- @section('content','This is Contact Page') --}}
+@section('content')
+
+<h1>Profile</h1>
+Lorem ipsum dolor sit <strong style= "color: red "">amet</strong>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+@endsection
