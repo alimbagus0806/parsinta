4b6541025d66d676070cca0cb8d4384be299fc2c
Bagus <alimbagus0806@gmail.com>
2023-01-30 17:44:28 +0700

    Belajar Route Resource

--- newproject/routes/web.php
+++ newproject/routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\ProfileInformationController;
 Route::get ('/', function() {
     return view('home');
 } );
-
+// Route::resource('contact', ContactController::class);
 // Route::get('/', HomeController::class);
 Route::get('contact', [ContactController::class, 'create']);
 Route::post('contact', [ContactController::class, 'store']);
@@ -30,12 +30,12 @@ Route::get('profile', [ProfileInformationController::class, '__invoke']);
 Route::view ('about', 'about');
 // Route::view ('blog', 'blog');
 // Route::view ('posts/first-post', 'posts.show');
+Route::resource('tasks', TaskController::class);
+// Route::get ('tasks', [TaskController::class, 'index']);
+// Route::get ('tasks/create', [TaskController::class, 'create']);
 
-Route::get ('tasks', [TaskController::class, 'index']);
-Route::get ('tasks/create', [TaskController::class, 'create']);
+// Route::post ('tasks', [TaskController::class, 'store']);
 
-Route::post ('tasks', [TaskController::class, 'store']);
-
-Route::get ('tasks/{id}/edit', [TaskController::class, 'edit']);
-Route::put ('tasks/{id}', [TaskController::class, 'update']);
-Route::delete ('tasks/{id}', [TaskController::class, 'destroy']);
+// Route::get ('tasks/{id}/edit', [TaskController::class, 'edit']);
+// Route::put ('tasks/{id}', [TaskController::class, 'update']);
+// Route::delete ('tasks/{id}', [TaskController::class, 'destroy']);
